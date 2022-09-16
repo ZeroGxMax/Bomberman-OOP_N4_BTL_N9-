8e5d8fd2fc140acd0a9c1b59f66a2b1a090c9852
Phạm Anh Tuấn <21020397@vnu.edu.vn>
2022-09-16 15:29:06 +0700

    Project initialization

---
+++ src/entities/Wall.java
@@ -0,0 +1,15 @@
+package entities;
+
+import javafx.scene.image.Image;
+
+public class Wall extends Entity {
+
+    public Wall(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
