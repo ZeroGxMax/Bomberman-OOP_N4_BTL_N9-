9f0148dd199c4f6c7ca2defc66cd0638e434e9c0
ZeroGxMax <21020554@vnu.edu.vn>
2022-09-29 22:34:30 +0700

    Add new package: constants +, Add GAME_TITLE +, Add DIRECTION enum class

---
+++ src/constants/constants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class constants {
+    public static final String GAME_TITLE = "Bomberman";
+    public enum DIRECTION {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+}
