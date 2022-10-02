b50d94b47fed5fb53cc2553952623a187e9e2c0d
ZeroGxMax <21020554@vnu.edu.vn>
2022-10-02 14:55:29 +0700

    Add new package: Tracing +, Các Enemy sẽ di chuyển theo phương thức trong tracing.

---
+++ src/tracing/RandomTracing.java
@@ -0,0 +1,28 @@
+package tracing;
+
+import constants.Constants.DIRECTION;
+
+import java.util.Random;
+
+public class RandomTracing extends Tracing {
+    public static final int TIME_EACH_DIRECTION_MAX = 100;
+    protected static Random random = new Random();
+    public int timeEachDirection = 0;
+
+    @Override
+    public DIRECTION calculateDirection() {
+        int randomNumber = random.nextInt() % 4;
+        switch (randomNumber) {
+            case 0:
+                return DIRECTION.UP;
+            case 1:
+                return DIRECTION.RIGHT;
+            case 2:
+                return DIRECTION.DOWN;
+            case 3:
+                return DIRECTION.LEFT;
+            default:
+                return DIRECTION.NONE;
+        }
+    }
+}
---
+++ src/tracing/Tracing.java
@@ -0,0 +1,11 @@
+package tracing;
+
+import constants.Constants.DIRECTION;
+
+import java.util.Random;
+
+public abstract class Tracing {
+
+    public abstract DIRECTION calculateDirection();
+
+}
